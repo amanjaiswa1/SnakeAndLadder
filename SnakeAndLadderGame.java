646fb7a0c8e360613fdd82dd2ad5f2c983a3bce4
amanjaiswa1 <amanjaiswal0608@gmail.com>
2022-09-07 21:06:04 +0530

    Add: Checks For A Option

--- SnakeAndLadderGame.java
+++ SnakeAndLadderGame.java
@@ -3,13 +3,32 @@ package com.assignments.day4.SnakeAndLadder;
 import java.util.*;
 
 public class SnakeAndLadderGame {
-	static int P1 = 0;
+
     public static void main(String[] args) {
-        System.out.println("Welcome Player 1");
-        Random ran = new Random();
-        int roll = ran.nextInt(5);
-        int out = roll + 1;
-        System.out.println("Player 1 rolled => "+out);
-        System.out.println("PLayer 1 is at position => "+P1 + out);
-    }
+    	 System.out.println("Welcome Player 1.");
+         int p1 = 0;
+         Random ran = new Random();
+         int roll = ran.nextInt(5);
+         int out = roll + 1;
+         int play = ran.nextInt(3);
+         int move = 0;
+         System.out.println("Player 1 rolled => "+out);
+         
+         if (play == 1) {
+             move = move - out;
+             p1 = move;
+             System.out.println("SNAKE: Player 1 found a Snake and moved behind.");
+             System.out.println("PLayer 1 is at position => "+p1);
+         } 
+         else if (play == 2) {
+             move = move + out;
+             p1 = move;
+             System.out.println("LADDER: Player 1 moved ahead using Ladder.");
+             System.out.println("PLayer 1 is at position => "+p1);
+         }
+         else {
+             System.out.println("NO PLAY: Player 1 can't move forward.");
+             System.out.println("PLayer 1 is at position => "+p1);
+         }
+     }
 }
