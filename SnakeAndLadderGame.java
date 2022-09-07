8ac895712b4c2147722029e1e78dba481bc09fa4
amanjaiswa1 <amanjaiswal0608@gmail.com>
2022-09-07 23:30:56 +0530

    Modify: Added Second Player

--- SnakeAndLadderGame.java
+++ SnakeAndLadderGame.java
@@ -3,48 +3,99 @@ package com.assignments.day4.SnakeAndLadder;
 import java.util.*;
 
 public class SnakeAndLadderGame {
-	static int counter = 0;
+	static int playerOneDiceCount = 0;
+	static int playerTwoDiceCount = 0;
+	
 	static void game() {
 		int p1 = 0;
-	    while (p1 < 100) {
+		int p2 = 0;
+		Random ran = new Random();
+		while (p1 < 100 && p2 < 100) {
+		// PLAYER 1
+			
+	    while (true) {
 	             if (p1 < 0) {
 	                 p1 = 0;
-	                 continue;
 	             }
-	         Random ran = new Random();
-	         int roll = ran.nextInt(5);
-	         int out = roll + 1;
-	         if (p1 + out > 100) {
-	                continue;
+	                int conditionPlayerOne = ran.nextInt(4);
+	                int playerOneDiceRoll = ran.nextInt(1, 6);
+	                playerOneDiceCount++;
+	                System.out.println("PLayer 1 is at position => " + p1);
+	                System.out.println("Player 1 rolled => " + playerOneDiceRoll);
+	                if (p1 + playerOneDiceRoll > 100) {
+	                    break;
+	                }
+	                if (conditionPlayerOne == 1) {
+	                    p1 = p1 - playerOneDiceRoll;
+	                    System.out.println("Player 1 found a Snake and moves backward.");
+	                    break;
+	                } else if (conditionPlayerOne == 2) {
+	                    p1 = p1 + playerOneDiceRoll;
+	                    System.out.println("Player 1 moves forward.");
+	                    break;
+	                } else if (conditionPlayerOne == 3) {
+	                    p1 = p1 + playerOneDiceRoll;
+	                    System.out.println("Player 1 found a Ladder and plays again.");
+	                    continue;
+	                } else {
+	                    System.out.println("Player 1 can't proceed.");
+	                }
+	                break;
 	            }
-	         int play = ran.nextInt(3);
-	
-	         if (play == 1) {
-	             p1 = p1 - out;
-	             System.out.println("Player 1 rolled => " + out);
-	             System.out.println("SNAKE: Player 1 found a Snake and moved behind.");
-	             } 
-	         else if (play == 2) {
-	             p1 = p1 + out;
-	             System.out.println("Player 1 rolled => " + out);
-	             System.out.println("LADDER: Player 1 moved ahead using Ladder.");
-	             }
-	         else {
-	        	 System.out.println("Player 1 rolled => " + out);
-	             System.out.println("NO PLAY: Player 1 can't move forward.");
-	         }
-	         counter++;
-	         System.out.println("Number of Die rolls by PLayer 1 => "+counter);
-	         System.out.println("PLayer 1 is at position => " + p1);
-	         System.out.println("    ");
-	     }
-	   
-	}
+	            System.out.println("PLayer 1 is at position => " + p1);
+	            System.out.println("    ");
+	            if (p1 == 100) {
+	                break;
+	            }
+
+	            // PLAYER 2
+
+	            while (true) {
+	                if (p2 < 0) {
+	                    p2 = 0;
+	                }
+	                int conditionPlayerTwo = ran.nextInt(4);
+	                int playerTwoDiceRoll = ran.nextInt(1,6);
+	                playerTwoDiceCount++;
+	                System.out.println("PLayer 2 is at position => " + p2);
+	                System.out.println("Player 2 rolled => " + playerTwoDiceRoll);
+	                if (p2 + playerTwoDiceRoll > 100) {
+	                    break;
+	                }
+	                if (conditionPlayerTwo == 1) {
+	                    p2 = p2 - playerTwoDiceRoll;
+	                    System.out.println("Player 2 found a Snake and moves backward.");
+	                    break;
+	                } else if (conditionPlayerTwo == 2) {
+	                    p2 = p2 + playerTwoDiceRoll;
+	                    System.out.println("Player 2 moves forward.");
+	                    break;
+	                } else if (conditionPlayerTwo == 3) {
+	                    p2 = p2 + playerTwoDiceRoll;
+	                    System.out.println("Player 2 found a Ladder and plays again.");
+	                    continue;
+	                } else {
+	                    System.out.println("Player 2 can't proceed.");
+	                }
+	                break;
+	            }
+	            System.out.println("PLayer 2 is at position => " + p2);
+	            System.out.println("    ");
+	        }
+	        if (p1 == 100) {
+	            System.out.println("Player 1 Won the Game.");
+	            System.out.println("Total number of Die rolled by PLayer 1 => " + playerOneDiceCount);
+	        } else {
+	            System.out.println("Player 2 Won the Game.");
+	            System.out.println("Total number of Die rolled by Player 2 => " + playerTwoDiceCount);
+	        }
+		}   
+		
 	
     public static void main(String[] args) {
     	 System.out.println("Welcome Player 1.");
+    	 System.out.println("Welcome Player 2.");
+    	 System.out.println("");
          game();
-         System.out.println("Player 1 has completed the Game.");
-         System.out.println("Total number of Die rolled => "+counter);
 }
 }
