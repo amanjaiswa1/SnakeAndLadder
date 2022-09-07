34c128377c15fb57875efa142b630e4041bef70c
amanjaiswa1 <amanjaiswal0608@gmail.com>
2022-09-07 20:21:03 +0530

    Add: Player Die Roll

--- SnakeAndLadderGame.java
+++ SnakeAndLadderGame.java
@@ -1,9 +1,15 @@
 package com.assignments.day4.SnakeAndLadder;
 
+import java.util.*;
+
 public class SnakeAndLadderGame {
 	static int P1 = 0;
     public static void main(String[] args) {
         System.out.println("Welcome Player 1");
-        System.out.println("PLayer 1 is at position => "+P1);
+        Random ran = new Random();
+        int roll = ran.nextInt(5);
+        int out = roll + 1;
+        System.out.println("Player 1 rolled => "+out);
+        System.out.println("PLayer 1 is at position => "+P1 + out);
     }
 }
