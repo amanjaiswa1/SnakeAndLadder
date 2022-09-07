7c34366ad30689d3f29e637cd0102cba922d02e8
amanjaiswa1 <amanjaiswal0608@gmail.com>
2022-09-07 20:12:25 +0530

    Add: Single Player At Start Position 0

---
+++ SnakeAndLadderGame.java
@@ -0,0 +1,9 @@
+package com.assignments.day4.SnakeAndLadder;
+
+public class SnakeAndLadderGame {
+	static int P1 = 0;
+    public static void main(String[] args) {
+        System.out.println("Welcome Player 1");
+        System.out.println("PLayer 1 is at position => "+P1);
+    }
+}
