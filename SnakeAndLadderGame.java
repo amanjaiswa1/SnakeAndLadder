b9b072f359eb268b0b62394899c0ab2afbb152e3
amanjaiswa1 <amanjaiswal0608@gmail.com>
2022-09-07 21:39:18 +0530

    Modify: Till The Player Win

--- SnakeAndLadderGame.java
+++ SnakeAndLadderGame.java
@@ -3,32 +3,43 @@ package com.assignments.day4.SnakeAndLadder;
 import java.util.*;
 
 public class SnakeAndLadderGame {
-
+	
+	static void game() {
+		int p1 = 0;
+	    while (p1 < 100) {
+	             if (p1 < 0) {
+	                 p1 = 0;
+	                 continue;
+	             }
+	         Random ran = new Random();
+	         int roll = ran.nextInt(5);
+	         int out = roll + 1;
+	         int play = ran.nextInt(3);
+	        //System.out.println(roll+" "+out);
+	         
+	         if (play == 1) {
+	             p1 = p1 - out;
+	             System.out.println("Player 1 rolled => " + out);
+	             System.out.println("SNAKE: Player 1 found a Snake and moved behind.");
+	             } 
+	         else if (play == 2) {
+	             p1 = p1 + out;
+	             System.out.println("Player 1 rolled => " + out);
+	             System.out.println("LADDER: Player 1 moved ahead using Ladder.");
+	             }
+	         else {
+	        	 System.out.println("Player 1 rolled => " + out);
+	             System.out.println("NO PLAY: Player 1 can't move forward.");
+	         }
+	         System.out.println("PLayer 1 is at position => " + p1);
+	         System.out.println("    ");
+	         p1 += 0;
+	     }
+	}
+	
     public static void main(String[] args) {
     	 System.out.println("Welcome Player 1.");
-         int p1 = 0;
-         Random ran = new Random();
-         int roll = ran.nextInt(5);
-         int out = roll + 1;
-         int play = ran.nextInt(3);
-         int move = 0;
-         System.out.println("Player 1 rolled => "+out);
-         
-         if (play == 1) {
-             move = move - out;
-             p1 = move;
-             System.out.println("SNAKE: Player 1 found a Snake and moved behind.");
-             System.out.println("PLayer 1 is at position => "+p1);
-         } 
-         else if (play == 2) {
-             move = move + out;
-             p1 = move;
-             System.out.println("LADDER: Player 1 moved ahead using Ladder.");
-             System.out.println("PLayer 1 is at position => "+p1);
-         }
-         else {
-             System.out.println("NO PLAY: Player 1 can't move forward.");
-             System.out.println("PLayer 1 is at position => "+p1);
-         }
-     }
+         game();
+         System.out.println("Player 1 has completed the Game.");
+}
 }
